30e4654f52df7508cad2e105315b54aebe950c2a
Joana <joana09@gmail.com>
2019-10-12 17:05:30 +0100

    init

---
+++ RequestValidationMiddleware/RequestValidation.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RequestValidationMiddleware
+{
+    public class RequestValidation
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestValidation(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            const string RegExInvalidCharacters = @"[\^&<>\""/]";
+            Regex rx = new Regex(RegExInvalidCharacters,
+     RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+            if (context.Request.Method == "POST")
+            {
+                var form = await context.Request.ReadFormAsync();
+                foreach (var item in form.Keys)
+                {
+                    if (rx.Matches(form[item]).Count > 0)
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("Invalid characters");
+                        return;
+                    }
+                }
+            }
+            else if (context.Request.Method == "GET")
+            {
+                foreach (var item in context.Request.Query.Keys)
+                {
+                    if (rx.Matches(context.Request.Query[item]).Count > 0)
+                    {
+                        context.Response.StatusCode = 500;
+                        await context.Response.WriteAsync("Invalid characters");
+                        return;
+                    }
+                }
+            }
+            await _next(context);
+        }
+    }
+}
---
+++ RequestValidationMiddleware/RequestValidationExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace RequestValidationMiddleware
+{
+    public static class RequestValidationExtensions
+    {
+        public static IApplicationBuilder UseRequestValidation(
+           this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestValidation>();
+        }
+    }
+}
